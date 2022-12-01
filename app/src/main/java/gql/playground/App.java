95992e983b20fbe41814452c975f5c07f91b9d18
Ashley Wilson <ashley.wilson@calrec.com>
2022-12-01 16:40:48 +0000

    Add reactive streams

--- app/src/main/java/gql/playground/App.java
+++ app/src/main/java/gql/playground/App.java
@@ -1,5 +1,6 @@
 package gql.playground;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -7,16 +8,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.dataloader.BatchLoader;
 import org.dataloader.DataLoader;
 import org.dataloader.DataLoaderFactory;
 import org.dataloader.DataLoaderRegistry;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.execution.AsyncExecutionStrategy;
+import graphql.execution.SubscriptionExecutionStrategy;
 import graphql.execution.instrumentation.dataloader.DataLoaderDispatcherInstrumentation;
 import graphql.execution.instrumentation.dataloader.DataLoaderDispatcherInstrumentationOptions;
 import graphql.schema.DataFetcher;
@@ -27,12 +33,16 @@ import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Flowable;
 
 public class App {
     public static void main(String[] args) {
         String schema = "type Query { \n";
         schema += "  hello: String \n";
         schema += "  products: [Product] \n";
+        schema += "  product: Product \n";
         schema += "} \n";
         schema += "type Product { \n";
         schema += "  id: ID \n";
@@ -41,24 +51,25 @@ public class App {
         schema += "  cost: Float \n";
         schema += "  tax: Float \n";
         schema += "} \n";
+        schema += "type Subscription { \n";
+        schema += "  product: Product \n";
+        schema += "} \n";
 
         SchemaParser schemaParser = new SchemaParser();
         TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
 
-        BatchLoader<String, Object> productsBatchLoader = new BatchLoader<String, Object>() {
+        BatchLoader<String, Observable<Product>> productsBatchLoader = new BatchLoader<String, Observable<Product>>() {
             @Override
-            public CompletionStage<List<Object>> load(List<String> keys) {
-                Map<String, Map<String, Object>> data = new HashMap<>();
-                data.put("1", new HashMap<>(){{
-                    put("id", "1");
-                    put("name", "P1");
-                }});
-                data.put("2", new HashMap<>(){{
-                    put("id", "2");
-                    put("name", "P2");
-                }});
+            public CompletionStage<List<Observable<Product>>> load(List<String> keys) {
+                List<Product> data = new ArrayList<>();
 
-                return CompletableFuture.completedFuture((List)data.values().stream().toList());
+                for (int i = 1; i <= keys.size(); i++) {
+                    String ID = Integer.toString(i);
+                    String name = String.format("P%d", i);
+                    data.add(new Product(ID, name, Float.valueOf(0), Float.valueOf(0)));
+                }
+
+                return CompletableFuture.supplyAsync(() -> data.stream().map(Observable::just).toList());
             }
         };
 
@@ -70,14 +81,44 @@ public class App {
                         return CompletableFuture.completedFuture("world!");
                     }
                 })
-                .dataFetcher("products", new DataFetcher<CompletableFuture<List<Object>>>() {
+                .dataFetcher("products", new DataFetcher<CompletableFuture<List<Product>>>() {
                     @Override
-                    public CompletableFuture<List<Object>> get(DataFetchingEnvironment environment) {
-                        DataLoader<String, Object> dataLoader = environment.getDataLoader("products");
-                        return dataLoader.loadMany(Arrays.asList("1", "2"));
+                    public CompletableFuture<List<Product>> get(DataFetchingEnvironment environment) {
+                        DataLoader<String, Observable<Product>> dataLoader = environment.getDataLoader("products");
+
+                        return dataLoader.loadMany(Arrays.asList("1", "2"))
+                            .thenCompose(obs -> {
+                                return Observable.combineLatestArray(obs.stream().toArray(Observable[]::new), (entries) -> entries)
+                                .firstOrErrorStage();
+                            });
                     }
                 })
-            ).build();
+                .dataFetcher("product", new DataFetcher<CompletableFuture<Product>>() {
+                    @Override
+                    public CompletableFuture<Product> get(DataFetchingEnvironment environment) {
+                        DataLoader<String, Observable<Product>> dataLoader = environment.getDataLoader("products");
+
+                        return dataLoader.load("1").thenCompose(obs -> obs.firstOrErrorStage());
+                    }
+                })
+            )
+            .type("Subscription", builder -> builder
+                .dataFetcher("product", new DataFetcher<Publisher<Product>>() {
+                    @Override
+                    public Publisher<Product> get(DataFetchingEnvironment environment) {
+                        DataLoader<String, Observable<Product>> dataLoader = environment.getDataLoader("products");
+
+                        return Observable.fromCompletionStage(dataLoader.load("1"))
+                            .flatMap(x -> x)
+                            .doOnNext(msg -> {
+                                System.out.println("RECV -> MSG");
+                            })
+                            .take(1)
+                            .toFlowable(BackpressureStrategy.LATEST);
+                    }
+                })
+            )
+            .build();
 
         SchemaGenerator schemaGenerator = new SchemaGenerator();
         GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
@@ -89,16 +130,17 @@ public class App {
 
         GraphQL build = GraphQL.newGraphQL(graphQLSchema)
             .instrumentation(dispatcherInstrumentation)
-            // .queryExecutionStrategy(new AsyncExecutionStrategy())
-            // .subscriptionExecutionStrategy(new AsyncExecutionStrategy())
+            .queryExecutionStrategy(new AsyncExecutionStrategy())
+            .subscriptionExecutionStrategy(new SubscriptionExecutionStrategy())
             .build();
 
-        DataLoader<String, Object> productsDataLoader = DataLoaderFactory.newDataLoader(productsBatchLoader);
+        DataLoader<String, Observable<Product>> productsDataLoader = DataLoaderFactory.newDataLoader(productsBatchLoader);
         DataLoaderRegistry registry = new DataLoaderRegistry();
         registry.register("products", productsDataLoader);
 
         ExecutionInput executionInput = ExecutionInput.newExecutionInput()
-            .query("{ products { id, name, cost, tax }}")
+            // .query("subscription GET_PRODUCT { product { id, name, cost, tax }}")
+            .query("query GET_PRODUCT { product { id, name, cost, tax }}")
             .dataLoaderRegistry(registry)
             .build();
 
@@ -106,9 +148,53 @@ public class App {
         ExecutionResult executionResult = build.execute(executionInput);
 
         System.out.println("== DATA");
-        System.out.println(executionResult.getData().toString());
+        System.out.println((Object) executionResult.getData());
         System.out.println("** ERR");
         System.err.println(executionResult.getErrors());
+
+        // Publisher<ExecutionResult> dataStream = executionResult.getData();
+
+        // AtomicReference<Subscription> subscriptionRef = new AtomicReference<>();
+        // dataStream.subscribe(new Subscriber<ExecutionResult>() {
+
+        //     @Override
+        //     public void onSubscribe(Subscription s) {
+        //         System.out.println("DO SUBSCRIBE");
+        //         subscriptionRef.set(s);
+        //         s.request(1);
+        //     }
+
+        //     @Override
+        //     public void onNext(ExecutionResult er) {
+        //         System.out.println("GOT ON NEXT DATA");
+        //         System.out.println((Object) er.getData());
+        //         subscriptionRef.get().request(1);
+        //     }
+
+        //     @Override
+        //     public void onError(Throwable t) {
+        //         System.out.println("DO ERROR");
+        //     }
+        
+        //     @Override
+        //     public void onComplete() {
+        //         System.out.println("DO COMPLETE");
+        //     }
+        // });
         // Prints: {hello=world}
+    }
+
+    public static class Product {
+        final String id;
+        final String name;
+        final Float cost;
+        final Float tax;
+
+        public Product(String id, String name, Float cost, Float tax) {
+            this.id = id;
+            this.name = name;
+            this.cost = cost;
+            this.tax = tax;
+        }
     }
 }
