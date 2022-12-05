7ec9e7a2c0dc496e50bdc39bd04ad638dac7b597
Ashley Wilson <scifilem@gmail.com>
2022-12-05 22:27:05 +0000

    Cache example

--- app/src/main/java/gql/playground/Loaders.java
+++ app/src/main/java/gql/playground/Loaders.java
@@ -1,11 +1,14 @@
 package gql.playground;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.dataloader.BatchLoaderContextProvider;
 import org.dataloader.BatchLoaderWithContext;
 import org.dataloader.DataLoader;
 import org.dataloader.DataLoaderFactory;
 import org.dataloader.DataLoaderOptions;
 import org.dataloader.DataLoaderRegistry;
+import org.dataloader.ValueCache;
 
 import akka.actor.typed.ActorSystem;
 import gql.playground.enums.PathType;
@@ -33,12 +36,40 @@ public class Loaders {
         }
     };
 
-    DataLoaderOptions loaderOptions = DataLoaderOptions.newOptions().setBatchLoaderContextProvider(contextProvider);
-    DataLoader<PathType, Observable<Fader>> fadersDataLoader = DataLoaderFactory.newDataLoader(fadersBatchLoader, loaderOptions);
-    DataLoader<String, Boolean> taxDataLoader = DataLoaderFactory.newDataLoader(isAccessedLoader, loaderOptions);
+    ValueCache<PathType, Observable<Fader>> fadersValueCache = new ValueCache<PathType, Observable<Fader>>() {
+        @Override
+        public CompletableFuture<Observable<Fader>> get(PathType key) {
+            System.out.println("CACHE GET :: "+ key);
+            return CompletableFuture.failedFuture(new Exception("Cache is empty"));
+        }
+
+        @Override
+        public CompletableFuture<Observable<Fader>> set(PathType key, Observable<Fader> value) {
+          System.out.println("CACHE SET :: "+ key + " -> "+ value);
+            return CompletableFuture.failedFuture(new Exception("Caching not supported"));
+        }
+    
+        @Override
+        public CompletableFuture<Void> delete(PathType key) {
+            return CompletableFuture.completedFuture(null);
+        }
+
+        @Override
+        public CompletableFuture<Void> clear() {
+            return CompletableFuture.completedFuture(null);
+        }
+    };
+
+    DataLoaderOptions fadersLoaderOptions = DataLoaderOptions.newOptions()
+      .setBatchLoaderContextProvider(contextProvider)
+      .setValueCache(fadersValueCache);
+    DataLoaderOptions isAccessedLoaderOptions = DataLoaderOptions.newOptions()
+      .setBatchLoaderContextProvider(contextProvider);
+    DataLoader<PathType, Observable<Fader>> fadersDataLoader = DataLoaderFactory.newDataLoader(fadersBatchLoader, fadersLoaderOptions);
+    DataLoader<String, Boolean> isAccessedDataLoader = DataLoaderFactory.newDataLoader(isAccessedLoader, isAccessedLoaderOptions);
     DataLoaderRegistry registry = new DataLoaderRegistry();
     registry.register(DataLoaders.FADERS.name(), fadersDataLoader);
-    registry.register(DataLoaders.ACCESSED.name(), taxDataLoader);
+    registry.register(DataLoaders.ACCESSED.name(), isAccessedDataLoader);
 
     return registry;
   }
