ef59dfe7f465300bcb88aa63a4e6975c4a2c78db
Ashley Wilson <scifilem@gmail.com>
2023-05-22 17:11:14 +0100

    feat: Backpressure aware source

--- app/src/main/java/gql/playground/BroadcastHubs.java
+++ app/src/main/java/gql/playground/BroadcastHubs.java
@@ -4,6 +4,10 @@ import static java.nio.ByteOrder.LITTLE_ENDIAN;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import akka.Done;
 import akka.NotUsed;
@@ -12,35 +16,37 @@ import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.japi.Pair;
-import akka.stream.OverflowStrategy;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
-import akka.stream.typed.javadsl.ActorSink;
 import akka.stream.typed.javadsl.ActorSource;
 import akka.util.ByteString;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 
 public class BroadcastHubs {
-  private static Integer NUM_MESSAGES = 20;
+  private static Integer NUM_MESSAGES = 50;
 
   public static void main(String[] args) {
     Behavior<ParentCommand> parent = Behaviors.setup(context -> {
-      Source<ByteString, ActorRef<ByteString>> mcsOutbound = ActorSource.actorRef(
-        msg -> false,
+      BlockingQueue<ByteString> buffer = new ArrayBlockingQueue<ByteString>(1024);
+      AtomicBoolean isBusy = new AtomicBoolean(false);
+
+      Source<ByteString, ActorRef<ByteString>> mcsOutbound = ActorSource.actorRefWithBackpressure(
+        context.getSelf(),
+        StreamState.ACK,
         msg -> Optional.empty(),
-        8,
-        OverflowStrategy.fail()
+        msg -> Optional.empty()
       );
 
       Pair<ActorRef<ByteString>, Source<ByteString, NotUsed>> outboundPair = mcsOutbound.preMaterialize(context.getSystem());
 
+      Sink<ByteString, CompletionStage<Done>> sink = Sink.foreach(message -> {
+        System.out.println(">>>>> MCS MSG -> "+ message.toString() + " on thread : "+ Thread.currentThread());
+      });
+
       outboundPair.second()
         .log("MCSClientHub")
-        .runWith(Sink.foreach(message -> {
-          // FIXME This sink cannot keep up with the source
-          System.out.println(">>>>> MCS MSG -> "+ message.toString() + " on thread : "+ Thread.currentThread());
-        }), context.getSystem());
+        .runWith(sink, context.getSystem());
 
       return Behaviors.receive(ParentCommand.class)
         .onMessage(SendExternalMessage.class, (command) -> {
@@ -48,7 +54,9 @@ public class BroadcastHubs {
           ByteBuf out = Unpooled.buffer().order(LITTLE_ENDIAN);
           out.writeByte(command.index);
 
-          outboundPair.first().tell(ByteString.fromByteBuffer(out.nioBuffer()));
+          buffer.add(ByteString.fromByteBuffer(out.nioBuffer()));
+          context.getSelf().tell(new SendNextMessage());
+
           return Behaviors.same();
         })
         .onMessage(SendMultiMessage.class, (command) -> {
@@ -60,7 +68,25 @@ public class BroadcastHubs {
               return ByteString.fromByteBuffer(out.nioBuffer());
             }).toList();
 
-          Source.from(messages).runWith(ActorSink.actorRef(outboundPair.first(), ByteString.emptyByteString(), msg -> null), context.getSystem());
+          buffer.addAll(messages);
+          context.getSelf().tell(new SendNextMessage());
+
+          return Behaviors.same();
+        })
+        .onMessage(SendNextMessage.class, ignore -> {
+          if (isBusy.get() == false) {
+            Optional.ofNullable(buffer.poll())
+              .ifPresent(message -> {
+                outboundPair.first().tell(message);
+              });
+            isBusy.set(true);
+          }
+
+          return Behaviors.same();
+        })
+        .onMessage(StreamState.class, ignore -> {
+          isBusy.set(false);
+          context.getSelf().tell(new SendNextMessage());
           return Behaviors.same();
         })
         .build();
@@ -82,5 +108,10 @@ public class BroadcastHubs {
   public static interface ParentCommand {}
   public static record SendExternalMessage(Integer index) implements ParentCommand {}
   public static record SendMultiMessage(List<Integer> indexes) implements ParentCommand {}
+  public static record MessageProcessed() implements ParentCommand {}
   public static record Stop() implements ParentCommand {}
+  public static record SendNextMessage() implements ParentCommand {}
+  public static enum StreamState implements ParentCommand {
+    ACK
+  }
 }
